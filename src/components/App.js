546114e22083a745ac50409e86dbda8ff115fc71
LPandorf <leticiapandorf@gmail.com>
2022-09-18 15:52:44 -0300

    vcbfc

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import Corpo from "./Corpo";
+import Navbar from "./Navbar";
+export default function App(){
+    return (
+        <div>
+            <Navbar/>
+            <Corpo/>
+        </div>
+    );
+}
