d02bcacf42153557b822d4c1b3644cea42a1028d
LPandorf <leticiapandorf@gmail.com>
2022-09-18 17:41:48 -0300

    post props

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,77 +1,49 @@
+function Post(props){
+    console.log(props);
+    return(
+        <div class="post">
+            <div class="topo">
+              <div class="usuario">
+                <img src={props.imagem} />
+                {props.usuario}
+              </div>
+              <div class="acoes">
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+              </div>
+            </div>
+
+            <div class="conteudo">
+              <img src={props.imgpostada} />
+            </div>
+
+            <div class="fundo">
+              <div class="acoes">
+                <div>
+                  <ion-icon name="heart-outline"></ion-icon>
+                  <ion-icon name="chatbubble-outline"></ion-icon>
+                  <ion-icon name="paper-plane-outline"></ion-icon>
+                </div>
+                <div>
+                  <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+              </div>
+
+              <div class="curtidas">
+                <img src={props.imgcurtiu} />
+                <div class="texto">
+                  Curtido por <strong>{props.curtidopor}</strong> e <strong>outras {props.pessoas} pessoas</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+    )
+}
+
 export default function Posts(){
     return (
         <div class="posts">
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/meowed.svg" />
-                meowed
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/gato-telefone.svg" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/respondeai.svg" />
-                <div class="texto">
-                  Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/barked.svg" />
-                barked
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/dog.svg" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Post usuario="meowed" imagem="assets/img/meowed.svg" imgpostada="assets/img/gato-telefone.svg" imgcurtiu="assets/img/respondeai.svg" curtidopor="respondeai" pessoas="101.523" />
+          <Post usuario="barked" imagem="assets/img/barked.svg" imgpostada="assets/img/dog.svg" imgcurtiu="assets/img/adorable_animals.svg" curtidopor="adorable_animals" pessoas="99.159" />
         </div>
     )
 }
