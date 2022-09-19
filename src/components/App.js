2ebc30dffbe7a7bb135bf7bb5e46d26489c1c949
LPandorf <leticiapandorf@gmail.com>
2022-09-19 14:40:54 -0300

    agr ta no lugar certo o rodapé

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,13 @@ export default function App(){
         <div>
             <Navbar/>
             <Corpo/>
+            <div class="fundo-mobile">
+                <ion-icon name="home"></ion-icon>
+                <ion-icon name="search-outline"></ion-icon>
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
         </div>
     );
 }
